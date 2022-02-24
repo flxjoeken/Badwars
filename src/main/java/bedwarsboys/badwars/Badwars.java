501eb56cb099c9e438f0f0e8b24ba55e2caa18e7
Chef Experte <aago2001@web.de>
2022-02-24 18:37:12 +0100

    Added config.yml Added static access variables in Badwars.java Added basic config support Partially implemented SetArenaCommand for Player input

--- src/main/java/bedwarsboys/badwars/Badwars.java
+++ src/main/java/bedwarsboys/badwars/Badwars.java
@@ -2,17 +2,24 @@ package bedwarsboys.badwars;
 
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Objects;
 
 public final class Badwars extends JavaPlugin {
 
-    String PLUGIN_NAME = "[Badwars] ";
+    public static Badwars PLUGIN;
+    public static final String PLUGIN_NAME = "[Badwars] ";
+    public static FileConfiguration CONFIG;
     CommandExecutor setArenaCommand = new SetArenaCommand();
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        this.getCommand("setarena").setExecutor(setArenaCommand);
+        PLUGIN = this;
+        CONFIG = this.getConfig();
+        Objects.requireNonNull(this.getCommand("setarena")).setExecutor(setArenaCommand);
         Bukkit.getLogger().info(PLUGIN_NAME+"Loaded Plugin.");
     }
 
--- src/main/java/bedwarsboys/badwars/SetArenaCommand.java
+++ src/main/java/bedwarsboys/badwars/SetArenaCommand.java
@@ -3,11 +3,48 @@ package bedwarsboys.badwars;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class SetArenaCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        return false;
+
+        if (sender instanceof Player p) {
+            int pos = Integer.parseInt(args[0]);
+            String w = p.getLocation().getWorld().getName();
+            double x,y,z;
+            if (args.length == 1 || args.length == 4) {
+                if (args.length == 1 && (args[0].equals("1") | args[0].equals("2"))) {
+                    //Save Player position as arena bounds 1 or 2
+                    w = p.getLocation().getWorld().getName();
+                    x = p.getLocation().getBlockX();
+                    y = p.getLocation().getBlockY();
+                    z = p.getLocation().getBlockZ();
+                } else {
+                    //Save given position as arena bounds 1 or 2
+                    x = Double.parseDouble(args[1]);
+                    y = Double.parseDouble(args[2]);
+                    z = Double.parseDouble(args[3]);
+                    Badwars.CONFIG.set("arena.pos" + args[0], w + "," + x + "," + y + "," + z);
+                    Badwars.PLUGIN.saveConfig();
+                    sender.sendMessage("Position " + args[0] + " saved successfully.");
+                }
+                Badwars.CONFIG.set("arena.pos" + pos, w + "," + x + "," + y + "," + z);
+                Badwars.PLUGIN.saveConfig();
+                sender.sendMessage("Position " + pos + " saved successfully.");
+                return true;
+            }
+            if (args.length == 6) {
+                //Save given positions as arena bounds 1 and 2
+                //TODO
+                sender.sendMessage("Not implemented yet.");
+                return true;
+            } else {
+                return false;
+            }
+        }
+        sender.sendMessage("This command is currently for Players only.");
+        return true;
     }
 }
