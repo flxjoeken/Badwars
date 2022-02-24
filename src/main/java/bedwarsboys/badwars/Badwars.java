a42192cb44e5a5804c10c465e576462f9051c0e2
felix <felix@joeken.eu>
2022-02-24 05:37:52 +0100

    The epic beginning

--- src/main/java/bedwarsboys/badwars/Badwars.java
+++ src/main/java/bedwarsboys/badwars/Badwars.java
@@ -1,13 +1,13 @@
 package bedwarsboys.badwars;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Badwars extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getLogger().info("Hallo ich habs geschaft");
     }
 
     @Override
