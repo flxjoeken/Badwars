90c6771be898ccb7e110c10e114dfc817de59027
Chef Experte <aaron.henning@stud.uni-heidelberg.de>
2022-02-25 00:22:36 +0100

    Added WorldBlockArea for: - Saving Block areas from 2 Locations or a 3d-Block-Array - Loading Block areas (TODO) Improved SetArenaCommand Added SaveArenaCommand for - Saving the Arena bounds set with "setarena" command Added new command to plugin.yml

--- src/main/java/bedwarsboys/badwars/Badwars.java
+++ src/main/java/bedwarsboys/badwars/Badwars.java
@@ -1,10 +1,13 @@
 package bedwarsboys.badwars;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public final class Badwars extends JavaPlugin {
@@ -13,6 +16,7 @@ public final class Badwars extends JavaPlugin {
     public static final String PLUGIN_NAME = "[Badwars] ";
     public static FileConfiguration CONFIG;
     CommandExecutor setArenaCommand = new SetArenaCommand();
+    CommandExecutor saveArenaCommand = new SaveArenaCommand();
 
     @Override
     public void onEnable() {
@@ -20,11 +24,31 @@ public final class Badwars extends JavaPlugin {
         PLUGIN = this;
         CONFIG = this.getConfig();
         Objects.requireNonNull(this.getCommand("setarena")).setExecutor(setArenaCommand);
-        Bukkit.getLogger().info(PLUGIN_NAME+"Loaded Plugin.");
+        Objects.requireNonNull(this.getCommand("savearena")).setExecutor(saveArenaCommand);
+        Bukkit.getLogger().info(PLUGIN_NAME + "Loaded Plugin.");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    public static Location loadArenaPosition(int pos) {
+        if (pos < 1 || pos > 2) {
+            return null;
+        }
+        if (!Badwars.CONFIG.isSet("arena.pos" + pos)) {
+            Bukkit.getLogger().info(Badwars.PLUGIN_NAME+"The positions for the arena are not set yet, so the area cannot be saved to a file.");
+            return null;
+        }
+        String[] values = Objects.requireNonNull(Badwars.CONFIG.getString("arena.pos" + pos)).split(",");
+        String name = values[0];
+        double x = Double.parseDouble(values[1]);
+        double y = Double.parseDouble(values[2]);
+        double z = Double.parseDouble(values[3]);
+        assert name != null;
+        World w = Bukkit.getWorld(name);
+        return new Location(w, x, y, z);
+    }
+
 }
---
+++ src/main/java/bedwarsboys/badwars/SaveArenaCommand.java
@@ -0,0 +1,34 @@
+package bedwarsboys.badwars;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+public class SaveArenaCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (args.length != 1) {
+            return false;
+        }
+        Location pos1 = Badwars.loadArenaPosition(1);
+        Location pos2 = Badwars.loadArenaPosition(2);
+        if (pos1 == null || pos2 == null) {
+            sender.sendMessage(Badwars.PLUGIN_NAME+"One of the two positions has not been set, the area cannot be saved.");
+            return true;
+        }
+        WorldBlockArea area = new WorldBlockArea(pos1, pos2);
+        sender.sendMessage(Badwars.PLUGIN_NAME+ "Saving area...");
+        if (!area.saveBlockArea(args[0], true)){
+            sender.sendMessage(Badwars.PLUGIN_NAME+ "An error occurred while saving.");
+        }else {
+            sender.sendMessage(Badwars.PLUGIN_NAME + "Successfully saved.");
+        }
+        return true;
+    }
+}
