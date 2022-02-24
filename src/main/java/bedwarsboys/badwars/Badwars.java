69796dea724456588f92b4ece6c215547cb6bf72
felix <felix@joeken.eu>
2022-02-24 05:11:19 +0100

    initial Commit

---
+++ src/main/java/bedwarsboys/badwars/Badwars.java
@@ -0,0 +1,17 @@
+package bedwarsboys.badwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Badwars extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
