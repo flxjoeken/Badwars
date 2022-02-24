27b74a29a8d3d54fd244de19ebc8ae8db9aabe67
Chef Experte <aaron.henning@stud.uni-heidelberg.de>
2022-02-25 00:21:29 +0100

    Added WorldBlockArea for: - Saving Block areas from 2 Locations or a 3d-Block-Array - Loading Block areas (TODO) Improved SetArenaCommand

--- src/main/java/bedwarsboys/badwars/SetArenaCommand.java
+++ src/main/java/bedwarsboys/badwars/SetArenaCommand.java
@@ -11,9 +11,12 @@ public class SetArenaCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
 
         if (sender instanceof Player p) {
+            if (args.length == 0) {
+                return false;
+            }
             int pos = Integer.parseInt(args[0]);
             String w = p.getLocation().getWorld().getName();
-            double x,y,z;
+            double x, y, z;
             if (args.length == 1 || args.length == 4) {
                 if (args.length == 1 && (args[0].equals("1") | args[0].equals("2"))) {
                     //Save Player position as arena bounds 1 or 2
@@ -28,23 +31,23 @@ public class SetArenaCommand implements CommandExecutor {
                     z = Double.parseDouble(args[3]);
                     Badwars.CONFIG.set("arena.pos" + args[0], w + "," + x + "," + y + "," + z);
                     Badwars.PLUGIN.saveConfig();
-                    sender.sendMessage("Position " + args[0] + " saved successfully.");
+                    sender.sendMessage(Badwars.PLUGIN_NAME + "Position " + args[0] + " saved successfully.");
                 }
                 Badwars.CONFIG.set("arena.pos" + pos, w + "," + x + "," + y + "," + z);
                 Badwars.PLUGIN.saveConfig();
-                sender.sendMessage("Position " + pos + " saved successfully.");
+                sender.sendMessage(Badwars.PLUGIN_NAME + "Position " + pos + " saved successfully.");
                 return true;
             }
             if (args.length == 6) {
                 //Save given positions as arena bounds 1 and 2
                 //TODO
-                sender.sendMessage("Not implemented yet.");
+                sender.sendMessage(Badwars.PLUGIN_NAME + "Not implemented yet.");
                 return true;
             } else {
                 return false;
             }
         }
-        sender.sendMessage("This command is currently for Players only.");
+        sender.sendMessage(Badwars.PLUGIN_NAME + "This command is currently for Players only.");
         return true;
     }
 }
