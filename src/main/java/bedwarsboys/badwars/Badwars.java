b1c1a1c7afb9c531bacdae48f263d39705527d63
Chef Experte <aago2001@web.de>
2022-02-24 05:44:37 +0100

    Added SetArenaCommand (no implementation yet) Fixed Badwars.java Added Command to plugin.yml

--- src/main/java/bedwarsboys/badwars/Badwars.java
+++ src/main/java/bedwarsboys/badwars/Badwars.java
@@ -1,13 +1,19 @@
 package bedwarsboys.badwars;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Badwars extends JavaPlugin {
 
+    String PLUGIN_NAME = "[Badwars] ";
+    CommandExecutor setArenaCommand = new SetArenaCommand();
+
     @Override
     public void onEnable() {
-        Bukkit.getLogger().info("Hallo ich habs geschaft");
+        // Plugin startup logic
+        this.getCommand("setarena").setExecutor(setArenaCommand);
+        Bukkit.getLogger().info(PLUGIN_NAME+"Loaded Plugin.");
     }
 
     @Override
