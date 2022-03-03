333000daa69f54181b79cfb5502885462ab2d7dd
Chef Experte <aaron.henning@stud.uni-heidelberg.de>
2022-03-03 22:08:40 +0100

    Added item Spawners - loading spawners from blocks in the saved area named "arena" - activating spawners - deactivation spawners

--- src/main/java/bedwarsboys/badwars/LobbyManager.java
+++ src/main/java/bedwarsboys/badwars/LobbyManager.java
@@ -4,14 +4,98 @@ import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import bedwarsboys.badwars.WorldBlockArea.VirtualBlock;
+import org.bukkit.entity.Item;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitTask;
+import org.bukkit.util.Vector;
+
 public class LobbyManager {
 
     Location lobbyLocation;
-    WorldBlockArea areaBackup;
+    WorldBlockArea lobbyBackup;
+
+    static ArrayList<Location> copperSpawner = new ArrayList<>();
+    static ArrayList<Location> ironSpawner = new ArrayList<>();
+    static ArrayList<Location> goldSpawner = new ArrayList<>();
+    static ArrayList<Location> specialSpawner = new ArrayList<>();
+
+    static ArrayList<BukkitTask> activeSpawners = new ArrayList<>();
 
     public LobbyManager() {
         loadLobbyLocation();
         backupLobbyLocation();
+    }
+
+    public boolean loadSpawners() {
+        copperSpawner.clear();
+        ironSpawner.clear();
+        goldSpawner.clear();
+        WorldBlockArea arena = new WorldBlockArea();
+        if (arena.loadBlockArea("arena")) {
+            for (VirtualBlock[][] b2 : arena.blocks) {
+                for (VirtualBlock[] b1 : b2) {
+                    for (VirtualBlock b : b1) {
+                        if (b.blockData.getMaterial() == Material.COPPER_BLOCK) {
+                            copperSpawner.add(b.location.clone().add(0.5, 1.5, 0.5));
+                            //Bukkit.getLogger().info("GOT COPPER BLOCK");
+                        }
+                        if (b.blockData.getMaterial() == Material.IRON_BLOCK) {
+                            ironSpawner.add(b.location.clone().add(0.5, 1.5, 0.5));
+                        }
+                        if (b.blockData.getMaterial() == Material.GOLD_BLOCK) {
+                            goldSpawner.add(b.location.clone().add(0.5, 1.5, 0.5));
+                        }
+                        if (b.blockData.getMaterial() == Material.CALCITE) {
+                            specialSpawner.add(b.location.clone().add(0.5, 1.5, 0.5));
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public void startSpawners() {
+        activeSpawners.add(Bukkit.getScheduler().runTaskTimer(Badwars.PLUGIN, () -> {
+            for (Location l : copperSpawner) {
+                Item drop = l.getWorld().spawn(l, Item.class);
+                drop.setItemStack(new ItemStack(Material.COPPER_INGOT));
+                drop.setVelocity(new Vector(0, 0.1, 0));
+            }
+        }, 30, 30));
+        activeSpawners.add(Bukkit.getScheduler().runTaskTimer(Badwars.PLUGIN, () -> {
+            for (Location l : ironSpawner) {
+                Item drop = l.getWorld().spawn(l, Item.class);
+                drop.setItemStack(new ItemStack(Material.IRON_INGOT));
+                drop.setVelocity(new Vector(0, 0.1, 0));
+            }
+        }, 20*8, 20*8));
+        activeSpawners.add(Bukkit.getScheduler().runTaskTimer(Badwars.PLUGIN, () -> {
+            for (Location l : goldSpawner) {
+                Item drop = l.getWorld().spawn(l, Item.class);
+                drop.setItemStack(new ItemStack(Material.GOLD_INGOT));
+                drop.setVelocity(new Vector(0, 0.1, 0));
+            }
+        }, 20*20, 20*20));
+        activeSpawners.add(Bukkit.getScheduler().runTaskTimer(Badwars.PLUGIN, () -> {
+            for (Location l : specialSpawner) {
+                Item drop = l.getWorld().spawn(l, Item.class);
+                drop.setItemStack(new ItemStack(Material.GUNPOWDER));
+                drop.setVelocity(new Vector(0, 0.1, 0));
+            }
+        }, 20*90, 20*90));
+    }
+
+    public void stopSpawners() {
+        for (BukkitTask t : activeSpawners) {
+            t.cancel();
+        }
+        activeSpawners.clear();
     }
 
     void loadLobbyLocation() {
@@ -37,8 +121,8 @@ public class LobbyManager {
             loadLobbyLocation();
             if (lobbyLocation == null) return false;
         }
-        areaBackup = new WorldBlockArea(lobbyLocation.clone().add(-4, -1, -4), lobbyLocation.clone().add(4, 5, 4));
-        if (!areaBackup.saveBlockArea("lobby_area_orig", true)) {
+        lobbyBackup = new WorldBlockArea(lobbyLocation.clone().add(-4, -1, -4), lobbyLocation.clone().add(4, 5, 4));
+        if (!lobbyBackup.saveBlockArea("lobby_area_orig", true)) {
             Bukkit.getLogger().info(Badwars.PLUGIN_NAME + "Couldn't backup the lobby area!");
         } else {
             //we were able to backup the area, perfect.
@@ -79,7 +163,7 @@ public class LobbyManager {
     }
 
     public void removeLobbyContainer() {
-        areaBackup.pasteArea();
+        lobbyBackup.pasteArea();
     }
 
 }
