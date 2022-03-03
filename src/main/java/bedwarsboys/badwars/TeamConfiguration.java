03ac3c9dafdf405493670a58ec730f852c8f550a
Chef Experte <aaron.henning@stud.uni-heidelberg.de>
2022-03-03 01:44:56 +0100

    Implemented SetTeamSpawn Command (not tested)

--- src/main/java/bedwarsboys/badwars/TeamConfiguration.java
+++ src/main/java/bedwarsboys/badwars/TeamConfiguration.java
@@ -82,7 +82,7 @@ public class TeamConfiguration {
 
     private static void doTeamAction(Player p, int slot) {
         if (menu.getContents()[slot] != null)
-            p.sendMessage("You clicked "+slot+"!");
+            p.sendMessage("You clicked " + slot + "!");
     }
 
     private static void toggleTeamActive(Player p, int slot) {
@@ -136,11 +136,54 @@ public class TeamConfiguration {
         }
     }
 
-    public static class SetTeamSpawnPosCommand implements CommandExecutor {
+    public static class SetTeamSpawnCommand implements CommandExecutor {
 
         @Override
         public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-            return false;
+            if (sender instanceof Player p) {
+                if (args.length == 1) {
+                    // see if input is a number
+                    boolean isNum = false;
+                    int num = -1;
+                    try {
+                        // if input is a number, assign it to num
+                        num = Integer.parseInt(args[0]);
+                        // if (num <= 7) isNum = true;
+                    } catch (NumberFormatException ignored) {
+                    }
+                    // check if input is a colors name
+                    int textNum = switch (args[0]) {
+                        case "red" -> 0;
+                        case "blue" -> 1;
+                        case "green" -> 2;
+                        case "yellow" -> 3;
+                        case "pink" -> 4;
+                        case "purple" -> 5;
+                        case "black" -> 6;
+                        case "white" -> 7;
+                        default -> -1;
+                    };
+                    if (textNum != -1) {
+                        num = textNum;
+                    }
+                    // assign the spawnpoint for given team if input was valid
+                    if (num != -1) {
+                        Badwars.CONFIG.set("teams." + num + ".x", p.getLocation().getBlockX());
+                        Badwars.CONFIG.set("teams." + num + ".y", p.getLocation().getBlockY());
+                        Badwars.CONFIG.set("teams." + num + ".z", p.getLocation().getBlockZ());
+                        Badwars.PLUGIN.saveConfig();
+                        sender.sendMessage(Badwars.PLUGIN_NAME+"Successfully saved spawnpoint.");
+                    } else {
+                        sender.sendMessage(Badwars.PLUGIN_NAME+"Input was not valid.");
+                    }
+                    return true;
+
+
+                }
+                return false;
+            }
+            sender.sendMessage(Badwars.PLUGIN_NAME+"This command is currently only for players.");
+            return true;
         }
     }
 
