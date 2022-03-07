735d5a264989e16896e61db0bd8ae8bf5d2ddcbc
Chef Experte <aago2001@web.de>
2022-03-07 01:35:19 +0100

    Moved commands to own file

---
+++ src/main/java/bedwarsboys/badwars/commands/ConfigureTeamsCommand.java
@@ -0,0 +1,18 @@
+package bedwarsboys.badwars.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class ConfigureTeamsCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        //TODO: Show inventory for specific GameConfig
+        if (!(sender instanceof Player)) return false;
+        invMenu.showToPlayer((Player) sender);
+        return true;
+    }
+}
