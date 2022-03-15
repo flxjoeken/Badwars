067f74f57024ee1cbe603658c1be236fe6143aa6
felix <felix@joeken.eu>
2022-03-15 01:47:09 +0100

    added some debug Commands to Bedwars_DebugCommand

--- src/main/java/bedwarsboys/badwars/commands/Bedwars_DebugCommand.java
+++ src/main/java/bedwarsboys/badwars/commands/Bedwars_DebugCommand.java
@@ -4,10 +4,13 @@ import bedwarsboys.badwars.Badwars;
 import bedwarsboys.badwars.game.Game;
 import bedwarsboys.badwars.game.GameConfig;
 import bedwarsboys.badwars.itemspawner.SpawnerConfig;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
 public class Bedwars_DebugCommand implements CommandExecutor {
@@ -29,6 +32,15 @@ public class Bedwars_DebugCommand implements CommandExecutor {
 //            SpawnerConfig.stopSpawners();
 //            return true;
 //        }
+        if (args[0].equalsIgnoreCase("makeShop"))  {
+            if (sender instanceof Player) {
+                for (int i = 0; i < ((Player) sender).getInventory().getSize(); i++) {
+                    ItemStack item = ((Player) sender).getInventory().getItem(i);
+                    if (item != null)
+                        sender.sendMessage("ItemStack item" + i + " = new ItemStack(Material." + item.getType() +");");
+                }
+            }
+        }
         if (args[0].equalsIgnoreCase("stopGame")) {
             sender.sendMessage(Badwars.PLUGIN_NAME + "das erste Spiel wird nun gestoppt");
             Game.games.get(0).stopGame();
