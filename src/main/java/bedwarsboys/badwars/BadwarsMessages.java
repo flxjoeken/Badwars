ae55e48e6a66eac1a478f3ae764a3379672b57d3
Chef Experte <aago2001@web.de>
2022-03-15 01:57:26 +0100

    Improved message strings Reformatted message strings Minor improvements on text based config

--- src/main/java/bedwarsboys/badwars/BadwarsMessages.java
+++ src/main/java/bedwarsboys/badwars/BadwarsMessages.java
@@ -3,38 +3,43 @@ package bedwarsboys.badwars;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 
 public class BadwarsMessages {
 
     //configure Teams procedure Chat Text.
     public static final TextComponent BEGIN_MESSAGE_1 = Component
-            .text(Badwars.PLUGIN_NAME + "Herzlich Willkommen im Spiel-Konfigurations-Generator. Möchtest du die Konfiguration " +
-                    "abbrechen, kannst du das jederzeit mit").append(Component.text(" exit ")
+            .text(Badwars.PLUGIN_NAME + "Herzlich Willkommen im Spiel-Konfigurations-Generator. Möchtest du " +
+                    "die Konfiguration abbrechen, kannst du das jederzeit mit").append(Component.text(" exit ")
                     .color(NamedTextColor.RED)).append(Component.text("im Chat tun."));
     public static final TextComponent BEGIN_MESSAGE_2 = Component
-            .text(Badwars.PLUGIN_NAME + "Als erstes kannst du auswählen, wie viele Teams und welche Teamfarben du in dieser " +
-                    "Konfiguration verwenden möchtest. Um zu beginnen, schreibe ")
+            .text(Badwars.PLUGIN_NAME + "Als erstes kannst du auswählen, wie viele Teams und welche " +
+                    "Teamfarben du in dieser Konfiguration verwenden möchtest. Um zu beginnen, schreibe ")
             .append(Component.text("weiter").color(NamedTextColor.GREEN)).append(Component.text("."));
-    public static final TextComponent SELECT_TEAM_COLOR_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Wähle nun das Team aus, das du " +
-            "konfigurieren möchtest. Schreibe dazu den Namen eines der folgenden Teams in den Chat: ");
-    public static final TextComponent ADD_SPAWN_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Stelle dich nun auf den " +
-                    "Wiedereinstiegspunkt des Teams und tippe ")
+    public static final TextComponent SELECT_TEAM_COLOR_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Wähle " +
+            "nun das Team aus, das du konfigurieren möchtest. Schreibe dazu den Namen eines der folgenden " +
+            "Teams in den Chat: ");
+    public static final TextComponent ADD_SPAWN_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Sehr gut. " +
+                    "Stelle dich nun auf den Wiedereinstiegspunkt des Teams und tippe ")
             .append(Component.text("spawn").color(NamedTextColor.GREEN));
-    public static final TextComponent ADDED_SPAWN_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Der Wiedereinstiegspunkt von " +
-            "Team %0 wurde erfolgreich erfasst");
-    public static final TextComponent ADD_BED_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Klicke nun auf das Bett des Teams");
-    public static final TextComponent ADD_TEAM_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Möchtest du ein weiteres Team hinzufügen dann tippe ")
-            .append(Component.text("weiter ").color(NamedTextColor.RED))
+    public static final TextComponent ADDED_SPAWN_MESSAGE = Component.text(Badwars.PLUGIN_NAME +
+            "Danke. Der Wiedereinstiegspunkt von Team %0 wurde erfolgreich erfasst");
+    public static final TextComponent ADD_BED_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Klicke nun auf " +
+            "das Bett des Teams");
+    public static final TextComponent ADD_TEAM_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Wunderbar. " +
+                    "Möchtest du ein weiteres Team hinzufügen dann tippe ")
+            .append(Component.text("weiter ").color(NamedTextColor.GREEN))
             .append(Component.text("sonst tippe "))
             .append(Component.text("fertig ").color(NamedTextColor.RED))
             .append(Component.text("."));
-    public static final TextComponent ADD_SPAWNER_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Nun kannst du die Spawner des Spiels " +
-                    "konfigurieren. Stelle dich dazu auf den Ort an dem die Ressourcen erscheinen sollen und tippe")
-            .append(Component.text("gold").color(NamedTextColor.RED))
+    public static final TextComponent ADD_SPAWNER_MESSAGE = Component.text(Badwars.PLUGIN_NAME +
+                    "Nun kannst du die Spawner des Spiels konfigurieren. Stelle dich dazu auf den Ort an dem die " +
+                    "Ressourcen erscheinen sollen und tippe")
+            .append(Component.text("gold").color(NamedTextColor.GOLD))
             .append(Component.text(", "))
-            .append(Component.text("eisen").color(NamedTextColor.RED))
+            .append(Component.text("eisen").color(NamedTextColor.GRAY))
             .append(Component.text(" oder "))
-            .append(Component.text("bronze").color(NamedTextColor.RED))
+            .append(Component.text("bronze").color(TextColor.color(72, 45, 20)))
             .append(Component.text(" um den Spawner zu erstellen. "));
     public static final TextComponent ADDED_SPAWNER_MESSAGE = Component.text(Badwars.PLUGIN_NAME + "Ein ");
     public static final TextComponent ADDED_SPAWNER_MESSAGE1 = Component.text(" wurde erfolgreich erstellt. ")
